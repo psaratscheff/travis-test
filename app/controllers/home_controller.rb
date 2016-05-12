c120b5994821714c4f3b518827b1cd5587818532
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-12 19:36:48 -0300

    local psql config + route file finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
+  end
+
+  def instagram_tag
+    render json: {Implementado: false}
   end
 end
