d551a77a3f9755a4dca49a087b109fd0a39e0544
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-13 19:41:00 -0300

    update version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   $access_token = '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
-  $version = 'v1.0.0'
+  $version = 'v1.0.1'
 end
