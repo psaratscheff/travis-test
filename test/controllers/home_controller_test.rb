fa3ff17d81f4ae37e60ed037cb24d485b59023bb
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-13 00:57:41 -0300

    update instagram tag search test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
-
+# Every single method tester here
 class HomeControllerTest < ActionController::TestCase
-
-  test "should get index" do
+  test 'should get index' do
     get :index
-    assert_response :success, msg: "ERROR 1: No se puede obtener el index"
+    assert_response :success, msg: 'ERROR 1: No se puede obtener el index'
   end
 
-  test "should get json" do
-    post :instagram_tag
-    assert_response :success, msg: "ERROR 2: No se puede obtener el instagram_tag"
+  test 'should get json' do
+    post :instagram_tag, tag: 'saratscheff'
+    assert_response :success, msg: 'ERROR 2: No se puede obtener el instagram_tag'
   end
-
 end
