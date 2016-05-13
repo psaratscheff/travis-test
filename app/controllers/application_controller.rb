b98701c4dce3b821070c97da7c095d7ce49c7fc0
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-13 00:44:24 -0300

    request a instagram funcionando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+# Global variables are defined here
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  $access_token = '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+# The only controller
 class HomeController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
@@ -5,6 +6,39 @@ class HomeController < ApplicationController
   end
 
   def instagram_tag
-    render json: {Implementado: false}
+    require 'httparty'
+
+    tag = params[:tag]
+    access_token = params[:access_token] ||= $access_token
+
+    # TODO: ACTIVAR EL CHEQUEO DE ACCESS_TOKEN!!
+    if tag.nil? # || access_token.nil?
+      render json: { error: "Please enter the parameters 'tag' and 'access_token'" }, status: 400
+      return
+    end
+
+    begin
+      result = insta_recent_media(tag, access_token)
+      render json: result
+    rescue => ex
+      logger.error ex.message
+      puts 'error 1001'
+      render json: { error: ex.message }, status: 503
+    end
+  end
+
+  private
+
+  def insta_recent_media(tag, access_token)
+    url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?'
+    url += 'access_token=' + access_token # + 'count=' + 10
+    response = HTTParty.get(url)
+
+    JSON.parse(response.body)
   end
 end
+
+# , headers:
+# {
+#   'Content-Type' => 'application/json',
+# })
