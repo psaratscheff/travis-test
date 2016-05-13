6d97f331e70e8aa72f36546cd30cee6eabd527ff
Pedro Saratscheff <pasaratscheff@uc.cl>
2016-05-12 21:11:51 -0300

    testing errors

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response :success, msg: "ERROR 1: No se puede obtener el index"
+  end
+
+  test "should get json" do
+    post :instagram_tag
+    assert_response :success, msg: "ERROR 2: No se puede obtener el instagram_tag"
+  end
+
+  test "assert true" do
+    assert true, msg: "ERROR 0a: ¡¡¡¡¡¡ ESTO JAMÁS DEBERÍA FALLAR !!!!!!"
+  end
+
+  test "assert false" do
+    assert false, msg: "ERROR 0b: Es un assert false, es normal que falle"
   end
 
 end
